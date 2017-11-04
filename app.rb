a197ce55c7c5051b7375f05827289c0b0c1a9621
Lisa Lee <snickers495@gmail.com>
2017-11-04 18:24:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,17 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get "/" do 
+      erb :root 
+    end 
+
+    get "/new" do 
+      erb :new 
+    end 
+
+    post "/pirates" do 
+      erb :shows
+    end
 
   end
 end
