a178566216afa4ff1ccc9dfec1e8ef1cb35d6cb4
Lisa Lee <snickers495@gmail.com>
2017-11-04 18:42:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,6 +13,11 @@ module FormsLab
     end
 
     post "/pirates" do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end 
+      @ships = Ship.all
       erb :show
     end
 
